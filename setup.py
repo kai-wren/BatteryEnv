98a0e7f252e725feca2471db2518ad4953574fef
Kai Wren <miguel.manukyanez@gmail.com>
2020-07-12 15:16:01 +0300

    minor changes in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_foo',
+setup(name='gym_BatteryEnv',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'pandas', 'matplotlib']  # And any other dependencies foo needs
 )
