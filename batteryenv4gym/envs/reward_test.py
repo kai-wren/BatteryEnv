6bcb291bf9ec3080e71a8334dd74fde8fc225678
Kai Wren <miguel.manukyanez@gmail.com>
2020-07-13 02:13:15 +0300

    test adjusted and moved

--- batteryenv4gym/envs/reward_test.py
+++ batteryenv4gym/envs/reward_test.py
@@ -1,5 +1,5 @@
 import unittest
-from batteryenv4gym.envs.battery_env import BatteryEnv
+from battery_env import BatteryEnv
 
 def reward_equationMAX(actual_load_list):
     return max(actual_load_list)
@@ -7,7 +7,7 @@ def reward_equationMAX(actual_load_list):
 class MyTest(unittest.TestCase):
     def test(self):
         env = BatteryEnv(reward_func=reward_equationMAX)
-        self.assertIn(env.action_space.sample, [0, 1, 2])
+        self.assertIn(env.action_space.sample(), [0, 1, 2])
         
-#if __name__ == '__main__':
-#    unittest.main()
+if __name__ == '__main__':
+    unittest.main()
