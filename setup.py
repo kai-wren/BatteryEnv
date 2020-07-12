92c2dffd68ab99aa4e2063e2875397b7ecbef870
Kai Wren <miguel.manukyanez@gmail.com>
2020-07-12 15:44:29 +0300

    smal fixes for setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_BatteryEnv',
+setup(name='BatteryEnv',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'pandas', 'matplotlib']  # And any other dependencies foo needs
 )
